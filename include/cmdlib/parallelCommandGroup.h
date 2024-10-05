2f89c092546f9fd96a6ab2278901ab44fb9b172f
Jayden-Boomer <89496979+Jayden-Boomer@users.noreply.github.com>
2024-10-04 20:24:01 -0500

    parallel/sequential groups

---
+++ include/cmdlib/parallelCommandGroup.h
@@ -0,0 +1,10 @@
+#ifndef __PARALLEL_COMMAND_GROUP_H__
+#define __PARALLEL_COMMAND_GROUP_H__
+
+#include "commandGroup.h"
+
+class ParallelCommandGroup : public CommandGroup {
+
+};
+
+#endif
---
+++ include/cmdlib/sequentialCommandGroup.h
@@ -0,0 +1,11 @@
+#ifndef __SEQUENTIAL_COMMAND_GROUP_H__
+#define __SEQUENTIAL_COMMAND_GROUP_H__
+
+#include "commandGroup.h"
+
+class ParallelCommandGroup : public CommandGroup {
+
+};
+
+
+#endif
